2ceb4757086cedc29eca034b184a7bcd650e8b2f
Henrique Rodrigues <ricknet10@gmail.com>
2023-07-03 18:57:26 -0300

    first

---
+++ BackEnd/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using BackEnd.Data;
+using BackEnd.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+         private readonly StoreContext _context;
+        public ProductController(StoreContext context)
+        {
+          _context = context;
+        }
+          [HttpGet]
+        public async Task<ActionResult<List<Product>>>GetProducts(){
+          // var product = await Products.ToListAsync();
+           return   await _context.Products.ToListAsync();
+            //return Ok(products);
+        } 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>>GetProduct(int id){
+           // return context.Product.Find(id);
+         return await _context.Products.FindAsync(id);
+
+
+        }
+    }
+}
---
+++ BackEnd/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BackEnd.Entities;
+using System;
+
+namespace BackEnd.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
