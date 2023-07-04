207f9254d69de57cf5d06ec1317853ddb5993158
ricknet10 <ricknet10@gmail.com>
2023-07-04 13:06:44 -0300

    App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,9 @@ import './App.css';
 import {useEffect,useState} from "react";
 
 function App() {
-  const [products,setProducts]=useState([
-   {name:'product1', price: 100.00},
-   {name:'product2', price: 100.00},
+  const [products,setProducts]=useState<Product[]>([
+  // {name:'product1', price: 100.00},
+   //{name:'product2', price: 100.00},
   ]);
  useEffect(() =>{
   fetch('http://localhost:5000/api/products')
@@ -16,13 +16,25 @@ function App() {
   
  function addProduct(){
   setProducts(prevState => [...prevState,
-  {name: 'product'+ (prevState.length = 1),price:(prevState.length = 100)+100}])
-  
+  {
+    id: prevState.lenght + 101,
+    name: 'product' + (prevState.lenght +1),
+    price:(prevState.lenght * 100) + 100,
+    brand:'som brand',
+    description:'some description',
+    pictureUrl: 'htt://picsum.photos/200'
+    //name: 'product'+ (prevState.length = 1),price:(prevState.length = 100)+100}])
+  }])
  }
 
   return (
     <div>
      <h1>React Ecommerce</h1>
+     <ul>
+      {products.map(product => (
+        <li key={product.id}>{product.name} - {product.price}</li>
+      ))}
+     </ul>
     </div>
   );
 }
